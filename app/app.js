276cc22b2c837296732be41cab738643af03cb01
zendy <zendywongso@gmail.com>
2017-02-08 10:34:30 +1100

    Delete unused codes

--- app/app.js
+++ app/app.js
@@ -27,7 +27,6 @@ const todo = (state, action) => {
 };
 
 const todos = (state = [], action) => {
-  // console.log('test A');
   switch (action.type) {
     case 'ADD_TODO':
       return [
@@ -42,7 +41,6 @@ const todos = (state = [], action) => {
 };
 
 const visibilityFilter = (state = 'SHOW_ALL', action) => {
-  // console.log('test B');
   switch (action.type) {
     case 'SET_VISIBILITY_FILTER':
       return action.filter;
@@ -52,92 +50,11 @@ const visibilityFilter = (state = 'SHOW_ALL', action) => {
 };
 
 const todoApp = combineReducers({
-  // todos: todos,
-  // visibilityFilter: visibilityFilter,
   todos,
   visibilityFilter,
 });
-// const todoApp = (state = {}, action) => {
-//   return {
-//     todos: todos(state.todos, action),
-//     visibilityFilter: visibilityFilter(state.visibilityFilter, action),
-//   };
-// };
 
 let store = createStore(todoApp);
-
-// console.group('Initial state:');
-// console.log(store.getState());
-// console.groupEnd();
-//
-// console.group('Dispatching ADD_TODO:');
-// store.dispatch({
-//   type: 'ADD_TODO',
-//   id: 0,
-//   text: 'Learn Redux',
-// });
-// console.log(store.getState());
-// console.groupEnd();
-//
-// console.group('Dispatching ADD_TODO:');
-// store.dispatch({
-//   type: 'ADD_TODO',
-//   id: 1,
-//   text: 'Go Do Stuff',
-// });
-// console.log(store.getState());
-// console.groupEnd();
-//
-// console.group('Dispatching TOGGLE_TODO:');
-// store.dispatch({
-//   type: 'TOGGLE_TODO',
-//   id: 1,
-// });
-// console.log(store.getState());
-// console.groupEnd();
-//
-// console.group('Dispatching SET_VISIBILITY_FILTER:');
-// store.dispatch({
-//   type: 'SET_VISIBILITY_FILTER',
-//   filter: 'SHOW_COMPLETED',
-// });
-// console.log(store.getState());
-// console.groupEnd();
-
-// let nextId = 0;
-// const addTodo = (value) => {
-//   store.dispatch({
-//     type: 'ADD_TODO',
-//     id: nextId++,
-//     text: value,
-//   });
-// };
-// const toggleTodo = (id) => {
-//   store.dispatch({
-//     type: 'TOGGLE_TODO',
-//     id,
-//   });
-// };
-// const toggleVisibilityFilter = (value) => {
-//   store.dispatch({
-//     type: 'SET_VISIBILITY_FILTER',
-//     filter: value,
-//   });
-// };
-
-// const render = () => {
-//   ReactDOM.render(
-//     <Provider store={createStore(todoApp)}>
-//       <TodoApp
-//         addTodo={addTodo}
-//         toggleTodo={toggleTodo}
-//         toggleVisibilityFilter={toggleVisibilityFilter}
-//       />
-//     </Provider>, document.getElementById('root'));
-// };
-
-// store.subscribe(render);
-// render();
 
 ReactDOM.render(
   <Provider store={store}>
