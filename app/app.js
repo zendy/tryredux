793b856ebcfbb3ce4b4d7ff186e5a9c5e0773d93
zendy <zendywongso@gmail.com>
2017-02-08 14:28:43 +1100

    Export reducers into separate file

--- app/app.js
+++ app/app.js
@@ -1,62 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createStore, combineReducers } from 'redux';
+import { createStore } from 'redux';
 import { Provider } from 'react-redux';
 
 import TodoApp from './containers/TodoApp';
-
-const todo = (state, action) => {
-  switch (action.type) {
-    case 'ADD_TODO':
-      return {
-        id: action.id,
-        text: action.text,
-        completed: false,
-      };
-    case 'TOGGLE_TODO':
-      if (state.id !== action.id) {
-        return state;
-      }
-
-      return Object.assign({}, state, {
-        completed: !state.completed,
-      });
-    default:
-      return state;
-  }
-};
-
-const todos = (state = [], action) => {
-  switch (action.type) {
-    case 'ADD_TODO':
-      return [
-        ...state,
-        todo({}, action),
-      ];
-    case 'TOGGLE_TODO':
-      return state.map(t => todo(t, action));
-    default:
-      return state;
-  }
-};
-
-const visibilityFilter = (state = 'SHOW_ALL', action) => {
-  switch (action.type) {
-    case 'SET_VISIBILITY_FILTER':
-      return action.filter;
-    default:
-      return state;
-  }
-};
-
-const todoApp = combineReducers({
-  todos,
-  visibilityFilter,
-});
-
-let store = createStore(todoApp);
+import TodoAppReducers from './containers/TodoApp.reducers';
 
 ReactDOM.render(
-  <Provider store={store}>
+  <Provider store={createStore(TodoAppReducers)}>
     <TodoApp />
   </Provider>, document.getElementById('root'));
