7f931fd81fa9e3a25da99eadabb970434f3010b5
zendy <zendywongso@gmail.com>
2017-02-08 14:49:00 +1100

    Add save to local storage, but comment it out

--- app/app.js
+++ app/app.js
@@ -6,7 +6,27 @@ import { Provider } from 'react-redux';
 import TodoApp from './containers/TodoApp';
 import TodoAppReducers from './containers/TodoApp.reducers';
 
+import { loadState, saveState } from './helpers/localStorage';
+
+
+const persistedState = {
+  todos: [
+    {
+      id: '0',
+      text: 'Welcome',
+      completed: false,
+    }],
+};
+// const persistedState = loadState();
+
+const store = createStore(TodoAppReducers, persistedState);
+// store.subscribe(() => (
+//   saveState({
+//     todos: store.getState().todos,
+//   })
+// ));
+
 ReactDOM.render(
-  <Provider store={createStore(TodoAppReducers)}>
+  <Provider store={store}>
     <TodoApp />
   </Provider>, document.getElementById('root'));
